2f26f8569265934d6139e29ff2c98596a592a61e
James Jeffery <jameslovescode@gmail.com>
2016-08-16 13:21:07 +0100

    Added logo

--- app/js/hello.js
+++ app/js/hello.js
@@ -1,5 +1,5 @@
 export default () => {
   let app = document.getElementById('app')
-  app.innerHTML = '<img src="http://i.imgur.com/4IGAAU4.png" width="200">'
+  app.innerHTML = '<img src="dist/img/logo.png" width="200">'
   app.innerHTML += '<h1>Welcome to Neuron!</h1>'
 }
