6e5de771c0a742ac644b34a93ed613207dd97c68
James Jeffery <jameslovescode@gmail.com>
2016-08-16 13:20:08 +0100

    Added logo

--- app/dist/app.js
+++ app/dist/app.js
@@ -21483,7 +21483,7 @@
 
 	exports.default = function () {
 	  var app = document.getElementById('app');
-	  app.innerHTML = '<img src="http://i.imgur.com/4IGAAU4.png" width="200">';
+	  app.innerHTML = '<img src="dist/img/logo.png" width="200">';
 	  app.innerHTML += '<h1>Welcome to Neuron!</h1>';
 	};
 
